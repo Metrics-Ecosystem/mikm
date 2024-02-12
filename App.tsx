2061fe3053e072c1da04ad36b5edbcc39de79755
skalexet <iamskalexet@gmail.com>
2024-02-12 21:55:34 +0300

    feat tap interaction

--- App.tsx
+++ App.tsx
@@ -4,27 +4,35 @@
  * @repository https://github.com/Metrics-Ecosystem/mikm.git
  */
 
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StyleSheet,
   TouchableOpacity,
   TextInput,
+  Text,
   View,
   Dimensions,
   StatusBar,
+  KeyboardAvoidingView,
+  Platform,
 } from 'react-native';
 // import i18n from './i18n' // localisation library
 
 /** Globals */
-const windowHeight = Dimensions.get('window').height;
+const windowHeight = Dimensions.get('window').height * 0.5;
 
 function App(): React.JSX.Element {
   
   /** States */
-  const [miValue, setMi] = useState('');
-  const [kmValue, setKm] = useState('');
+  const [miValue, setMi] = useState('1');
+  const [kmValue, setKm] = useState('1.62');
+  // const [dynamicStyles, setDynamicStyles]: any = useState(styles.noPoint);
+  
+  /** Refs */
+  const miInputRef = useRef<TextInput | null>(null);
+  const kmInputRef = useRef<TextInput | null>(null);
   
   /** Functions */
   const kmToMi = (km: any) => {
@@ -40,18 +48,37 @@ function App(): React.JSX.Element {
   return (
     <SafeAreaView style={styles.background}>
       <StatusBar hidden={true} />
+      <KeyboardAvoidingView 
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.dynamicWrap}>
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={styles.background}>
             <View style={styles.container}>
-              <TouchableOpacity style={styles.unitArea}>
-                <TextInput style={styles.unitInput} keyboardType='number-pad' value={miValue} onChangeText={miToKm} />
+              <TouchableOpacity>
+                <View style={styles.unitArea}>
+                <TextInput
+                ref={miInputRef}
+                style={[styles.unitInput]}
+                keyboardType='number-pad'
+                value={miValue}
+                onChangeText={miToKm}
+                />
+                </View>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.unitArea}>
-                <TextInput style={styles.unitInput} keyboardType='number-pad' value={kmValue} onChangeText={kmToMi} />
+              <TouchableOpacity>
+                <View style={styles.unitArea}>
+                  <TextInput 
+                  ref={kmInputRef}
+                  style={[styles.unitInput]}
+                  keyboardType='number-pad'
+                  value={kmValue}
+                  onChangeText={kmToMi} />
+                </View>
               </TouchableOpacity>
             </View>
       </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
@@ -61,26 +88,40 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
     height: '100%'
   },
+  dynamicWrap: {
+    flex: 1,
+    display: 'flex',
+    alignContent: 'center',
+    justifyContent: 'center'
+  },
   container: {
     flexDirection: 'column',
     alignItems: 'center',
     height: '100%',
-    position: 'relative'
+    position: 'relative',
+    display: 'flex',
+    alignContent: 'center',
+    justifyContent: 'center'
   },
   unitArea: {
-    backgroundColor: '#000000',
     textAlign: 'center',
     width: '100%',
     height: windowHeight / 2
   },
   unitInput: {
-    flex: 1,
+    flex: 2,
     fontSize: 62,
     fontWeight: 'bold',
-    height: 80,
     color: '#ffffff',
     textAlign: 'center',
-  }
+    width: 'auto'
+  },
+  // point: {
+  //   pointerEvents: 'auto'
+  // },
+  // noPoint: {
+  //   pointerEvents: 'none'
+  // }
 });
 
 export default App;
