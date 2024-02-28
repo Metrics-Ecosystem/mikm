6e4b3722a422257f91e45fc49489d9a7f1e98e3c
skalexet <iamskalexet@gmail.com>
2024-02-28 14:07:04 +0400

    fix test environment

--- __mocks__/react-native.js
+++ __mocks__/react-native.js
@@ -1,31 +1,3 @@
-// jest.mock('react-native', () => {
-//     const originalModule = jest.requireActual('react-native');
-//     return {
-//         ...originalModule,
-//         Platform: {
-//             OS: 'ios'
-//         },
-//         NativeModules: {
-//             SettingsManager: {
-//                 settings: {
-//                     AppleLocale: 'en_US',
-//                     AppleLanguages: ['en_US']
-//                 },
-//             },
-//             I18nManager: {
-//                 localeIdentifier: 'en_US'
-//             },
-//             ProgressBarAndroid: {},
-//             Clipboard: {},
-//         },
-//         Dimensions: {
-//             get: jest.fn().mockReturnValue({height: 800, width: 600}),
-//         },
-//     };
-// });
-
-// // jest.spyOn(console, 'warn').mockImplementation(warning => {
-// //     if (!warning.includes('ProgressBarAndroid') && !warning.includes('Clipboard')) {
-// //         console.warn(warning);
-// //     }
-// // });
+/**
+ * Mocks Jest Test
+ */
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,3 @@
 module.exports = {
   preset: 'react-native',
-  // setupFiles: ['./__mocks__/react-native.js']
 };
