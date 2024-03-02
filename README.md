027165d07fb73b1a11d7037479c4d23eacc70473
skalexet <iamskalexet@gmail.com>
2024-03-02 21:53:15 +0400

    fix readme

--- README.md
+++ README.md
@@ -1,7 +1,49 @@
-* Metrics Ecosystem
-* * MiKm
+# __Metrics Ecosystem__ / __MiKm__
 
-Minimalistic miles/kilometers converter mobile application delivering excellent experience to users who need fast and high quality solutions in their traveling, commuting, fitness and various daily tasks.
+__Name__: 
+- MiKm
 
-* * The App is available in 38 languages * * 
-English, Spanish, Portuguese, Chinese, Arabic, French, Russian, Japanese, Hindi, Bengali, Urdu, German, Swahili, Filipino, Italian, Dutch, Korean, Turkish, Vietnamese, Thai, Czech, Polish, Hungarian, Greek, Romanian, Indonesian, Malay, Finnish, Swedish, Danish, Norwegian, Hebrew, Icelandic, Slovak, Serbian, Lithuanian, Slovenian, Macedonian.
+__Promotional__:
+- Miles and Kilometers
+
+__Description__: 
+- The MiKm app is a swift way to get comfortable wether you are used to miles or kilometers. <br>Miles/Kilometers converter with a minimalistic and user-friendly design delivering seamless experience to users who need fast and high quality solutions in their traveling, commuting, fitness and various daily tasks. <br>
+
+CONVERT <br>Convert values quickly by simply entering values from your phone's keypad <br> 
+PINPOINT <br>Switch easily between short approximate and precise values in the required system as needed by clicking the centered "eye" button <br> 
+PASTE NO WASTE <br>Paste any copied values into the input without worrying about accidently selected characters such as "12.556mi!" -> "12.556" - the app will understand what you meant<br>
+ENJOY <br> MiKm has no advertisements embeeded, so enjoy the smooth experience while using our software
+
+Happy converting!
+
+* * The App is available in 30 languages:
+    Traditional Chinese,
+    Simplified Chinese,
+    English,
+    Spanish,
+    Portuguese,
+    French,
+    German,
+    Italian,
+    Swedish,
+    Dutch,
+    Danish,
+    Norwegian,
+    Finnish,
+    Polish,
+    Czech,
+    Greek,
+    Hungarian,
+    Romanian,
+    Slovak
+    Japanese,
+    Russian,
+    Hebrew,
+    Arabic,
+    Indonesian,
+    Malay,
+    Hindi,
+    Korean,
+    Turkish,
+    Vietnamese,
+    Thai,
--- i18n.ts
+++ i18n.ts
@@ -1,49 +1,41 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import reactNativeLanguageDetector from '@os-team/i18next-react-native-language-detector';
-import en from './locales/en.json';
-import es from './locales/es.json';
-import fr from './locales/fr.json';
-import de from './locales/de.json';
-import zh from './locales/zh.json';
-import ru from './locales/ru.json';
-import it from './locales/it.json';
-import pt from './locales/pt.json';
-import ja from './locales/ja.json';
-import ar from './locales/ar.json';
-import hi from './locales/hi.json';
-import bn from './locales/bn.json';
-import ur from './locales/ur.json';
-import sw from './locales/sw.json';
-import tl from './locales/tl.json';
-import nl from './locales/nl.json';
-import ko from './locales/ko.json';
-import tr from './locales/tr.json';
-import vi from './locales/vi.json';
-import th from './locales/th.json';
-import cs from './locales/cs.json';
-import pl from './locales/pl.json';
-import hu from './locales/hu.json';
-import el from './locales/el.json';
-import ro from './locales/ro.json';
-import id from './locales/id.json';
-import ms from './locales/ms.json';
-import fi from './locales/fi.json';
-import sv from './locales/sv.json';
-import da from './locales/da.json';
-import no from './locales/no.json';
-import he from './locales/he.json';
-import is from './locales/is.json';
-import sk from './locales/sk.json';
-import sr from './locales/sr.json';
-import lt from './locales/lt.json';
-import sl from './locales/sl.json';
-import mk from './locales/mk.json';
+import zhHant from './locales/zhHant.json'; //TRADITIONAL CHINESE
+import zhHans from './locales/zhHans.json'; //SIMPLIFIED CHINESE
+import en from './locales/en.json'; //ENGLISH
+import es from './locales/es.json'; //SPANISH
+import pt from './locales/pt.json'; //PORTUGUESE
+import fr from './locales/fr.json'; //FRENCH
+import de from './locales/de.json'; //GERMAN
+import it from './locales/it.json'; //ITALIAN
+import sv from './locales/sv.json'; //SWEDISH
+import nl from './locales/nl.json'; //DUTCH
+import da from './locales/da.json'; //DANISH
+import no from './locales/no.json'; //NORWEGIAN BOKMÅL
+import fi from './locales/fi.json'; //FINNISH
+import pl from './locales/pl.json'; //POLISH
+import cs from './locales/cs.json'; //CZECH
+import el from './locales/el.json'; //GREEK
+import hu from './locales/hu.json'; //HUNGARIAN
+import ro from './locales/ro.json'; //ROMANIAN
+import sk from './locales/sk.json'; //SLOVAK
+import ja from './locales/ja.json'; //JAPANESE
+import ru from './locales/ru.json'; //RUSSIAN
+import he from './locales/he.json'; //HEBREW
+import ar from './locales/ar.json'; //ARABIC
+import id from './locales/id.json'; //INDONESIAN
+import ms from './locales/ms.json'; //MALAY
+import hi from './locales/hi.json'; //HINDI
+import ko from './locales/ko.json'; //KOREAN
+import tr from './locales/tr.json'; //TURKISH
+import vi from './locales/vi.json'; //VIETNAMESE
+import th from './locales/th.json'; //THAI
 
 const resources = {
-  en,es,fr,de,zh,ru,it,pt,ja,ar,hi,bn,ur,sw,tl,nl,ko,tr,vi,
-  th,cs,pl,hu,el,ro,id,ms,fi,sv,da,no,he,is,sk,sr,lt,sl,mk,
-  // 38 languages
+  // 30 languages
+  zhHant,zhHans,en,es,pt,fr,de,it,sv,nl,da,no,fi,pl,
+  cs,el,hu,ro,sk,ja,ru,he,ar,id,ms,hi,ko,tr,vi,th
 };
 
 i18n
